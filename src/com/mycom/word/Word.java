a1ef5dc6799663922366c57c1a0070e0b8bf33c8
tmddls17 <junding9995@gmail.com>
2022-09-07 16:23:36 +0900

    start(), addWord(), toString(), listAll()

--- src/com/mycom/word/Word.java
+++ src/com/mycom/word/Word.java
@@ -39,5 +39,21 @@ public class Word {
 		this.meaning = meaning;
 	}
 	
+	/*
+	 * 1 * 		electric 	전기의, 전기를 생산하는
+	 * 2 * 		    pole 	기둥, 장대
+	 */
+	
+	@Override
+	public String toString() {
+		
+		String slevel ="";
+		for(int i = 0; i < level; i++)
+			slevel += "*";
+		String str = String.format("%-3s", slevel)
+				+ String.format("%15s", word) + "  " + meaning;
+		return str;
+	}
+	
 	
 }
