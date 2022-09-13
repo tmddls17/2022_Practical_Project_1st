daf13cdde6574ce504899e45025700306b4fcfe7
tmddls17 <junding9995@gmail.com>
2022-09-13 10:00:52 +0900

    updateItem() and deleteItem()

--- src/com/mycom/word/WordCRUD.java
+++ src/com/mycom/word/WordCRUD.java
@@ -59,5 +59,53 @@ public class WordCRUD implements ICRUD{
 		}
 		System.out.println("----------------------------");
 	}
+	
+	public ArrayList<Integer> listAll(String keyword) {
+		ArrayList<Integer> idlist = new ArrayList<>();
+		int j = 0;
+		System.out.println("----------------------------");
+		for(int i = 0; i < list.size(); i++) {
+			String word = list.get(i).getWord();
+			if(!word.contains(keyword)) continue;
+			System.out.print((j+1) + " ");
+			System.out.println(list.get(i).toString());
+			idlist.add(i);
+			j++;
+		}
+		System.out.println("----------------------------");
+		
+		return idlist;
+	}
+	
+	public void updateItem() {
+		System.out.print("=> 수정할 단어 검색: ");
+		String keyword = s.next();
+		ArrayList<Integer> idlist = this.listAll(keyword);
+		System.out.print("=> 수정할 번호 선택: ");
+		int id = s.nextInt();
+		s.nextLine();
+		
+		System.out.print("=> 뜻 입력: ");
+		String meaning = s.nextLine();
+		Word word = list.get(idlist.get(id-1));
+		word.setMeaning(meaning);
+		System.out.println("단어가 수정되었습니다.");
+	}
+	public void deleteItem() {
+		System.out.print("=> 삭제할 단어 검색: ");
+		String keyword = s.next();
+		ArrayList<Integer> idlist = this.listAll(keyword);
+		System.out.print("=> 삭제할 번호 선택: ");
+		int id = s.nextInt();
+		s.nextLine();
+		
+		System.out.print("=> 정말 삭제하시겠습니까?(Y/N): ");
+		String ans = s.next();
+		if(ans.equalsIgnoreCase("y")) {
+			list.remove(idlist.get(id-1));
+			System.out.println("단어가 삭제되었습니다.");
+		} else
+			System.out.println("취소되었습니다.");		
+	}
 
 }
--- src/com/mycom/word/WordManager.java
+++ src/com/mycom/word/WordManager.java
@@ -6,21 +6,6 @@ public class WordManager {
 	Scanner s = new Scanner(System.in); 
 	WordCRUD wordCRUD;
 	
-	/*
-	 *** 영단어 마스터 ***
-	 *******************
-	 1. 모든 단어 보기
-	 2. 수준별 단어 보기
-	 3. 단어 검색
-	 4. 단어 추가
-	 5. 단어 수정
-	 6. 단어 삭제
-	 7. 파일 저장
-	 0. 나가기
-	 *******************
-	 => 원하는 메뉴는?
-	 */
-	
 	WordManager() {
 		wordCRUD = new WordCRUD(s);
 		
@@ -44,14 +29,23 @@ public class WordManager {
 		while(true) {
 			int menu = selectMenu();
 			if(menu == 0) break;
-			if(menu == 4) {
-				//create
-				wordCRUD.addWord();
-			}
 			else if(menu == 1) {
 				//list
 				wordCRUD.listAll();
 			}
+			else if(menu == 4) {
+				//create
+				wordCRUD.addWord();
+			}
+			else if(menu == 5) {
+				//update
+				wordCRUD.updateItem();
+			}
+			else if(menu == 6) {
+				//delete
+				
+			}
+			
 		}
 	}
 }
