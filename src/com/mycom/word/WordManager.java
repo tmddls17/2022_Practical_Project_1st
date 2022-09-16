f3ef2ae1aeb5dbff3f024f65133b2c97bf6c0038
tmddls17 <junding9995@gmail.com>
2022-09-16 11:02:30 +0900

    last

--- src/com/mycom/word/WordManager.java
+++ src/com/mycom/word/WordManager.java
@@ -30,7 +30,10 @@ public class WordManager {
 		wordCRUD.loadFile();
 		while(true) {
 			int menu = selectMenu();
-			if(menu == 0) break;
+			if(menu == 0) {
+				System.out.println("\t*** 이용해주셔서 감사합니다. ***");
+				break;
+			}
 			else if(menu == 1) {
 				//list
 				wordCRUD.listAll();
